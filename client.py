bce5f0777e8e9b207afa2a14f03a009e5621dab9
tada <tada@unirobot.com>
2020-07-25 14:52:59 +0900

    名前入力に対応しました。速度が向上しました。非同期処理になりました。

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket
 from blockdef import BlockDef
 import json
 import random
+import threading
 target_ip = "ec2-54-248-128-220.ap-northeast-1.compute.amazonaws.com"
 target_port = 1234
 buffer_size = 8192
@@ -18,7 +19,11 @@ class gameLogic:
     def __init__(self):
         self.field=[[0]*20 for i in range(20)]
         self.KndBlock=BlockDef.definition
-        self.myblocks=readblock(self.KndBlock)
+        self.myblocks=[]
+        self.myblocks.append(readblock(self.KndBlock))
+        self.myblocks.append(readblock(self.KndBlock))
+        self.myblocks.append(readblock(self.KndBlock))
+        self.myblocks.append(readblock(self.KndBlock))
         self.turn=0
         self.Blockw=5
         self.Blockh=5
@@ -28,11 +33,12 @@ class gameLogic:
     def changeTurn(self):
         self.turn=(self.turn+1)%4
         cnt=0
-        return
         while not self.earyput(True):
             self.turn=(self.turn+1)%4
             cnt+=1
-            if cnt==4:exit()
+            if cnt==4:
+                print("gameOver")
+                exit()
 
 
     def isIn(self,x,y):
@@ -73,8 +79,7 @@ class gameLogic:
             if not targetBlock[i]:continue
             self.field[y+i//Blockw][x+i%Blockw]=color
         
-        if self.turn==self.myColor:
-            self.myblocks.remove(blockId)
+        self.myblocks[self.turn].remove(blockId)
         self.changeTurn()
         return True
 
@@ -97,10 +102,9 @@ class gameLogic:
         if self.turn==self.myColor:
             self.earyput(False)
     def earyput(self,isCheckMode):
-        l=self.getCanPutList()
+        l=self.getCanPutList2(self.turn+1)
         if isCheckMode and len(l)!=0:return True
         if isCheckMode: return False
-        print(len(l),self.myblocks)
 
         idx=random.randint(0,len(l)-1)
         data={}
@@ -109,7 +113,6 @@ class gameLogic:
         data["x"]=l[idx][2]
         data["y"]=l[idx][3]
         data["color"]=self.turn+1
-        print(json.dumps(data),"send_data")
         self.client.send(json.dumps(data).encode())
         return
 
@@ -133,7 +136,7 @@ class gameLogic:
 
     def getCanPutList(self,color):
         ans=[]
-        for num in self.myblocks:
+        for num in self.myblocks[color-1]:
             for i in range(900):
                 for spin in range(4):
                     if self.canPut(-3+i%30,-3+i//30,spin,num,color):
@@ -145,8 +148,6 @@ class gameLogic:
         sides=[[0,1],[1,0],[-1,0],[0,-1]]
         cross=[[1,1],[1,-1],[-1,1],[-1,-1]]
         edge_dic={0:[[19,19]],1:[[19,0]],2:[[0,19]],3:[[0,0]]}
-        import time
-        t1=time.time()
 
         for x in range(20):
             for y in range(20):
@@ -159,7 +160,7 @@ class gameLogic:
                         if flag:
                             edge_dic[i].append([x,y])
 
-        for num in self.myblocks:
+        for num in self.myblocks[color-1]:
             for spin in range(4):
                 for y in range(-1,6):
                     for x in range(-1,6):
@@ -207,22 +208,22 @@ class gameLogic:
 game=gameLogic()
 #game.easyDisp()
 
-game.randomizeField()
-game.easyDisp()
-print(sorted(game.getCanPutList2(1))==sorted(game.getCanPutList(1)))
-print(len(game.getCanPutList2(1)),len(game.getCanPutList(1)))
-import time
-t1=time.time()
-result2=game.getCanPutList2(1)
-t2=time.time()
-result=game.getCanPutList(1)
-t3=time.time()
-print(t3-t2,t2-t1)
-for i in result:
-    if i not in result2:
-        print(i)
+#game.randomizeField()
+#game.easyDisp()
+#print(sorted(game.getCanPutList2(1))==sorted(game.getCanPutList(1)))
+#print(len(game.getCanPutList2(1)),len(game.getCanPutList(1)))
+#import time
+#t1=time.time()
+#result2=game.getCanPutList2(1)
+#t2=time.time()
+#result=game.getCanPutList(1)
+#t3=time.time()
+#print(t3-t2,t2-t1)
+#for i in result:
+#    if i not in result2:
+#        print(i)
 
-exit()
+#exit()
 
 
 
@@ -233,20 +234,36 @@ tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # 2.サーバに接続
 tcp_client.connect((target_ip,target_port))
-
+print(dir(tcp_client))
+tcp_client.send(b"{\"UserName\":\"AI1\",\"Rate\":1500}")
 # サーバにデータを送信
 #tcp_client.send(b"Data by TCP Client!!")
 game.setclient(tcp_client)
 
-s=""
+
+message_list=[]
+
+def receiveFunc():
+    s=""
+    while True:
+        response=tcp_client.recv(buffer_size)
+        s+=response.decode()
+        if response.decode()[-1]!="\n":
+            continue
+        print(s)
+        message_list.append(s)
+        s=""
+    
+t1 = threading.Thread(target=receiveFunc)
+t1.setDaemon(True)
+t1.start()
 while True:
-  # サーバからのレスポンスを受信
-  response = tcp_client.recv(buffer_size)
-  s+=response.decode()
-  if response.decode()[-1]!="\n":
+
+  if len(message_list)==0:
     continue
-  print(s)
-  game.receiveMessage(s)
-  print("now turn ",game.turn)
-  print("[*]Received a response : {}".format(response))
-  s=""
+  
+  # サーバからのレスポンスを受信
+  game.receiveMessage(message_list[0])
+  print("now turn ",game.turn,len(message_list))
+  print("[*]Received a response : {}".format(message_list[0]))
+  message_list.pop(0)
